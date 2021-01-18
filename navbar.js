f2eb23a3f80f6951eb0fbed3a025d28c758ea524
Niranjan Raje <raje.niranjan18@siesgst.ac.in>
2021-01-18 11:26:12 +0530

    Initial commit

---
+++ navbar.js
@@ -0,0 +1,19 @@
+
+// Click on Explore button 
+
+const explore = document.getElementById('explore-btn');
+const sidebar = document.querySelector('.menu-list');
+const links = document.querySelectorAll('.links');
+
+// smooth transition
+explore.addEventListener('click',(event)=>{
+    sidebar.classList.toggle('open');
+})
+
+//close navbar if link is clicked
+
+links.forEach(item => {
+    item.addEventListener('click',()=>{
+        sidebar.classList.remove('open');
+    })
+})
